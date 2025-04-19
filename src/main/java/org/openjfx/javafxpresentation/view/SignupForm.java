4cedc9e302082ee20bc1f92c22eb358550c2a0a7
ajfait <jafait1004@gmail.com>
2025-04-18 20:15:47 -0500

    create signup form using scene builder

--- src/main/java/org/openjfx/javafxpresentation/view/SignupForm.java
+++ src/main/java/org/openjfx/javafxpresentation/view/SignupForm.java
@@ -13,7 +13,7 @@ public class SignupForm extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SignupForm.class.getResource("/org/openjfx/javafxpresentation/signup-form.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Alison's JavaFX Presentation");
         stage.setScene(scene);
         stage.show();
